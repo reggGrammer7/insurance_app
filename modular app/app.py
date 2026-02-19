08788b3623e491a6af9f3965c93580e2ab4a699b
Regent Yao Agbemafle <regent.ay7@gmail.com>
2026-02-19 01:07:19 -0700

    app details saved

--- modular app/app.py
+++ modular app/app.py
@@ -1,6 +1,6 @@
 # Import your modularized functions
-from data_loader import load_data
-from models.frequency import build_frequency_models
-from models.severity import build_severity_models
-from reserving.chain_ladder import chain_ladder
-from utils import rmse, build_lift_table
+from data import load_data
+from frequency import build_frequency_models
+from severity import build_severity_models
+from reserving import chain_ladder
+# from utils import rmse, build_lift_table
